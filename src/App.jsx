92a93cd8774a8f41c5a045e721ff5782d5e6c5cb
Anasssss1234 <anasq4628@gmail.com>
2024-07-29 12:16:55 +0530

    second message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import React from 'react'
+import './App.css'
+import Form from './Components/Form'
+function App() {
+  return (
+    <div>
+      <Form/>
+    </div>
+  )
+}
 
-
+export default App
 
 
 
